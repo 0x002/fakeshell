15ad7b8656d583d0e8e1a597e1219de35e7a2a9c
0x002 <jackk1973@hotmail.com>
2015-10-21 16:38:40 -0700

    attempt to implement builtin - exit

--- shell.py
+++ shell.py
@@ -7,7 +7,6 @@ import os
 while True:
   line = input('> ')
 
-
   # 3. builtins
   splitLine = line.split()
    
@@ -16,10 +15,19 @@ while True:
   
   # make the command replace the shell process
   if not pid: # child
+    # builtins
+
+    # cd
     if splitLine[0] == "cd":
       if len(splitLine) != 1:
-        os.chdir(splitLine[1]) 
+        os.chdir(splitLine[1])
+    
+    # exit
+    elif splitLine[0].startswith("exit"):
+      os.kill(pid, 9)
+      
     else:
       os.execvp(splitLine[0], splitLine)
+
   else:
     os.wait()
