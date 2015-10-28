00de97e1c22cc3635b3b0e9d41d486e2107a1bf0
Test <jackk1973@hotmail.com>
2015-10-28 16:19:20 -0700

    change to 4space

--- shell.py
+++ shell.py
@@ -6,34 +6,34 @@ import os
 
 
 def run():
-  while True:
-    builtin_commands = ['cd', 'exit']
+    while True:
+        builtin_commands = ['cd', 'exit']
 
-    line = raw_input('> ')
-    split_line = line.split()
-     
-    if split_line[0] in builtin_commands:
-      # builtins don't need to be forked
-      # cd
-      if split_line[0] == "cd":
-        if len(split_line) != 1:
-          os.chdir(split_line[1])
-      
-      # exit
-      elif split_line[0].startswith("exit"):
-        exit()    
-    
-    else:
-      pid = os.fork()
+        line = raw_input('> ')
+        split_line = line.split()
+         
+        if split_line[0] in builtin_commands:
+            # builtins don't need to be forked
+            # cd
+            if split_line[0] == "cd":
+                if len(split_line) != 1:
+                  os.chdir(split_line[1])
 
-      # make the command replace the shell process
-      if not pid: # child, pid = 0
-        os.execvp(split_line[0], split_line)
-      
-      else:
-        #print str(os.wait()) # see what process it is
-        os.wait()
+            # exit
+            elif split_line[0].startswith("exit"):
+                exit()    
+
+        else:
+            pid = os.fork()
+
+            # make the command replace the shell process
+            if not pid: # child, pid = 0
+                os.execvp(split_line[0], split_line)
+
+            else:
+                #print str(os.wait()) # see what process it is
+                os.wait()
 
 
 if __name__ == '__main__':
-  run()
+    run()
