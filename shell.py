d6f164b97d34d4d44933e91ed0e33841647e2bf7
0x002 <jackk1973@hotmail.com>
2015-10-27 22:21:56 -0700

    get rid of line numbers

--- shell.py
+++ shell.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
-  2 
-  3 # make shell
-  4 
-  5 import os
-  6 
-  7 while True:
-  8   line = raw_input('> ')
-  9 
- 10   # 3. builtins
- 11   splitLine = line.split()
- 12 
- 13   pid = os.fork()
- 14   #print pid
- 15 
- 16   # make the command replace the shell process
- 17   if not pid: # child, pid = 0
- 18     # builtins
- 19 
- 20     # cd
- 21     if splitLine[0] == "cd":
- 22       if len(splitLine) != 1:
- 23         os.chdir(splitLine[1])
- 24      
- 25     # exit
- 26     elif splitLine[0].startswith("exit"):
- 27       os.kill(pid, 9)
- 28       
- 29     else:
- 30       os.execvp(splitLine[0], splitLine)
- 31   
- 32     os._exit(0)
- 33   else:
- 34     print str(os.wait())
+
+# make shell
+
+import os
+
+while True:
+  line = raw_input('> ')
+
+  # 3. builtins
+  splitLine = line.split()
+   
+  pid = os.fork()
+  #print pid
+  
+  # make the command replace the shell process
+  if not pid: # child, pid = 0
+    # builtins
+
+    # cd
+    if splitLine[0] == "cd":
+      if len(splitLine) != 1:
+        os.chdir(splitLine[1])
+    
+    # exit
+    elif splitLine[0].startswith("exit"):
+      os.kill(pid, 9)
+      
+    else:
+      os.execvp(splitLine[0], splitLine)
+    
+    os._exit(0)
+  else:
+    print str(os.wait())
