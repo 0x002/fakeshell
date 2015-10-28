9fad07b97a0eeaf08c2beaf91ca07ad8e4960dd5
Test <jackk1973@hotmail.com>
2015-10-28 14:20:46 -0700

    attempt to fix cd bug

--- shell.py
+++ shell.py
@@ -4,31 +4,36 @@
 
 import os
 
-while True:
-  line = raw_input('> ')
 
-  # 3. builtins
-  splitLine = line.split()
-   
-  pid = os.fork()
-  #print pid
+def run():
+  while True:
+    builtin_commands = ['cd', 'exit']
+
+    line = raw_input('> ')
+    splitLine = line.split()
+     
+    pid = os.fork()
+
+    # make the command replace the shell process
+    if not pid: # child, pid = 0
+      if splitLine[0] not in builtin_commands:
+        os.execvp(splitLine[0], splitLine)
   
-  # make the command replace the shell process
-  if not pid: # child, pid = 0
-    # builtins
-
-    # cd
-    if splitLine[0] == "cd":
-      if len(splitLine) != 1:
-        os.chdir(splitLine[1])
+      os._exit(0)
     
-    # exit
-    elif splitLine[0].startswith("exit"):
-      os.kill(pid, 9)
-      
     else:
-      os.execvp(splitLine[0], splitLine)
-    
-    os._exit(0)
-  else:
-    print str(os.wait())
+      # builtins
+      # cd
+      if splitLine[0] == "cd":
+        if len(splitLine) != 1:
+          os.chdir(splitLine[1])
+      
+      # exit
+      elif splitLine[0].startswith("exit"):
+        exit()    
+      
+      print str(os.wait())
+
+
+if __name__ == '__main__':
+  run()
