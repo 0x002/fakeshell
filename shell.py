abf9bb2aa92f29d558a686daa8f66d88a84388c7
Test <jackk1973@hotmail.com>
2015-11-02 16:40:56 -0800

    WIP using dup2() for IO redirection

--- shell.py
+++ shell.py
@@ -8,10 +8,14 @@ import os
 def run():
     while True:
         builtin_commands = ['cd', 'exit']
+        redirecting_output = False
 
         line = raw_input('> ')
         split_line = line.split()
-         
+        
+        if ">" in split_line:
+            redirecting_output = True
+
         if split_line[0] in builtin_commands:
             # builtins don't need to be forked
             # cd
@@ -28,7 +32,16 @@ def run():
 
             # make the command replace the shell process
             if not pid: # child, pid = 0
-                os.execvp(split_line[0], split_line)
+                # if ">" in the command, only run part of the 
+                # input command(ignoring last two element in 
+                # arg array, > [filename])            
+                if redirecting_output:
+                    split_line_trimmed = split_line[:-2]
+                    os.execvp(split_line_trimmed[0], split_line_trimmed)
+
+
+                else:
+                    os.execvp(split_line[0], split_line)
 
             else:
                 #print str(os.wait()) # see what process it is
