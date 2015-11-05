1319d816578f84ad1e3e999dfe4ad0cefac347fa
Dev Account <devaccount@Zalbuntu.(none)>
2015-11-04 22:13:14 -0800

    update to IO redirect version without comments

--- shell.py
+++ shell.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+# make shell
+
 import os
+import sys
 
 def run():
     while True:
@@ -8,8 +11,14 @@ def run():
 
         line = raw_input('> ')
         split_line = line.split()
-         
+        
+        redirecting_output = False
+        if ">" in split_line:
+            redirecting_output = True
+            output_file = split_line[-1] 
+
         if split_line[0] in builtin_commands:
+
             if split_line[0] == "cd":
                 if len(split_line) != 1:
                   os.chdir(split_line[1])
@@ -20,12 +29,25 @@ def run():
         else:
             pid = os.fork()
 
+
             if not pid: 
-                os.execvp(split_line[0], split_line)
+
+                if redirecting_output:
+                    f = os.open(output_file, os.O_RDWR|os.O_CREAT)
+                    split_line_trimmed = split_line[:-2]                  
+                    os.dup2(f, sys.stdout.fileno())
+
+                    os.execvp(split_line_trimmed[0], split_line_trimmed)
+
+                else:
+                    os.execvp(split_line[0], split_line)
 
             else:
                 os.wait()
+                if redirecting_output:
+                    os.close(f)
 
 
 if __name__ == '__main__':
     run()
+
